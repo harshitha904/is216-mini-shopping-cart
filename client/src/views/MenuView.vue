3c5b172ecc8503185f7e15f56c48d0d27805228d
harshitha904 <harshitha.s.2025@computing.smu.edu.sg>
2026-09-21 15:17:49 +0800

    changes

--- client/src/views/MenuView.vue
+++ client/src/views/MenuView.vue
@@ -33,6 +33,25 @@ onMounted (async() => {
 // TODO: Fetch items for the currently selected category
 async function getItems() {
     // Add code
+
+    let url = "http://127.0.0.1:3000/items"
+
+    try {
+        let response = await axios.get(url,{
+            params: {
+                category : selected_category.value
+            }
+        })
+
+        items.value = response.data
+        console.log(items.value)
+
+        for (let item of items.value) {
+            item.quantity = 0
+        }
+    } catch(e) {
+        console.log(e.message)
+    }
 }
 
 // Add selected items to cart
@@ -50,6 +69,8 @@ function doAddToCart(itemsToAdd) {
         if (!found) cartItems.value.push(item);
     }
 
+    console.log(cartItems.value)
+
     // TODO: store current cartitems into local storage
     // cartItems.value is a JS (complex) obj. We need to use JSON.stringify to convert the JS obj to JSON string
    
@@ -63,7 +84,7 @@ function doAddToCart(itemsToAdd) {
   
     <!-- TODO: Category selection dropdown -->
     <label for="categories">Categories</label>
-    <select class="form-control" id="categories" >
+    <select class="form-control" id="categories" v-model="selected_category" v-on:change="getItems">
         <option v-for="cat in categories"> {{ cat }} </option>
     </select>
     <br>
@@ -72,10 +93,8 @@ function doAddToCart(itemsToAdd) {
     <div class="container-fluid">
         <div class="row p-3">
             <div class='col-md-6 text-center'>
-                <!-- TODO: Show Items using ItemsBrowser-->
-                <button>
-                    Add to Cart
-                </button>
+                <!-- TODO: Show Items using ItemsBrowser (IMP TAKE NOTE)--> 
+                 <ItemsBrowser v-bind:items v-on:addcart="doAddToCart">Add to Cart</ItemsBrowser>
             </div>
         </div>
 
